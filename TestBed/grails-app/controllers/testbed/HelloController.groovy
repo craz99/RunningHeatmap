776dba739d75da0d655ce20832ab3413e64f9921
Kendrick Burnett <kwb@veritegroup.com>
2014-10-06 08:48:11 -0400

    Initial map implementation with leaflet

--- TestBed/grails-app/controllers/testbed/HelloController.groovy
+++ TestBed/grails-app/controllers/testbed/HelloController.groovy
@@ -3,6 +3,18 @@ package testbed
 class HelloController {
 
     def index() {
-        render "Hello World!"
-    }
+		def gpsList = []
+		(1..2).each { i ->
+		  (1..2).each { j ->
+			  double latitude = 42.715103 + (0.00001*i)
+			  double longitude = -78.891551 + (0.00001*j)
+			  gpsList << new GPS(latitude:latitude, longitude:longitude)
+		  }
+		}
+		[ gpsList:gpsList ]
+	}
+	
+	def hi() {
+		render (view:'index.gsp')
+	}
 }
