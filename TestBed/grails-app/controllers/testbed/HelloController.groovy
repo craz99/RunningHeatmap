915d78efd1d3cfff76851547fe75c57960eda9fa
kendrickb@gmail.com <kwb@veritegroup.com>
2014-10-07 12:53:36 -0400

    Added ability to use geojson features with leaflet library

--- TestBed/grails-app/controllers/testbed/HelloController.groovy
+++ TestBed/grails-app/controllers/testbed/HelloController.groovy
@@ -1,18 +1,58 @@
 package testbed
 
+import grails.converters.JSON
+import groovy.json.JsonBuilder
+
 class HelloController {
 
     def index() {
-//		def gpsList = [new GPS(latitude:42.715103, longitude:-78.891551)]
-		def gpsList = []
+		def builder = new JsonBuilder()
+		def root = builder.type { LineString { coordinates( [-78.891551, 42.715103], [-78.890551, 42.714103] ) }}
+		def feature = [:]
+		feature['type'] = "Feature"
+		def properties = [:]
+		properties['name'] = "GeoTest"
+		feature['properties'] = properties
+		def result = [:]
+		result['type'] = "LineString"
+		
+		def coords = []
 		(1..2).each { i ->
 		  (1..2).each { j ->
 			  double latitude = 42.715103 + (0.01*i)
 			  double longitude = -78.891551 + (0.01*j)
-			  gpsList << new GPS(latitude:latitude, longitude:longitude)
+			  coords << [longitude, latitude]
 		  }
 		}
-		[ gpsList:gpsList ]
+		result['coordinates'] = coords
+		feature['geometry'] = result
+		[ geojson: new JSON(feature).toString() ]
+		//{"type":{"LineString":{
+		//"coordinates":[[42.715103,-78.891551],[42.714103,-78.890551]]}}
+		//}
+		//{ "type": "LineString",
+		//"coordinates": [ [100.0, 0.0], [101.0, 1.0] ]
+    	//}
+		
+		/**
+		 * //${gpsList.get(0).latitude};
+		//${gpsList.get(0).longitude};
+		//var latlngs = [];
+		//<g:each in="${gpsList}" var="gps">
+		//latlngs.push(L.latLng(${gps.latitude}, ${gps.longitude}));
+		//</g:each>
+		//var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+		 */
+		//		def gpsList = [new GPS(latitude:42.715103, longitude:-78.891551)]
+//		def gpsList = []
+//		(1..2).each { i ->
+//		  (1..2).each { j ->
+//			  double latitude = 42.715103 + (0.01*i)
+//			  double longitude = -78.891551 + (0.01*j)
+//			  gpsList << new GPS(latitude:latitude, longitude:longitude)
+//		  }
+//		}
+//		[ gpsList:gpsList ]
 	}
 	
 	def hi() {
--- TestBed/grails-app/views/hello/index.gsp
+++ TestBed/grails-app/views/hello/index.gsp
@@ -10,20 +10,15 @@
 <div id="map"></div>
 	<g:javascript src="leaflet.js" />
 	<script>
-		var lat = ${gpsList.get(0).latitude};
-		var lon = ${gpsList.get(0).longitude};
+		var lat = 42.725103; 
+		var lon = -78.891551; 
 		var map = L.map('map').setView([lat, lon], 13);
 		L.tileLayer('http://{s}.tiles.mapbox.com/v3/kendrickb.jmgka5g9/{z}/{x}/{y}.png', {
 		    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		    maxZoom: 18
 		}).addTo(map);
-		var latlngs = [];
-		<g:each in="${gpsList}" var="gps">
-		latlngs.push(L.latLng(${gps.latitude}, ${gps.longitude}));
-		</g:each>
-		var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-		
+		var data = ${raw(geojson)};
+		L.geoJson(data).addTo(map);
 	</script>
-
 </body>
 </html>
